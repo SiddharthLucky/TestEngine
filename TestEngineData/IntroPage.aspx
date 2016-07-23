eddcedacca484fe022274339d81fb26f88358047
Siddharth Reddy Lucky <siddharth.luckyreddy@gmail.com>
2016-07-22 22:41:50 -0600

    This is the final design for the Master Page with the placeholders defined

---
+++ TestEngineData/IntroPage.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="IntroPage.aspx.cs" Inherits="TestEngineData.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainHeaderPlaceHolder" runat="server">
+    This is a place holder test
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="CenterPlcaeHolder1" runat="server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="CenterPlaceHolderList" runat="server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="CenterPlaceHolder2" runat="server">
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="TitlePlaceHolder" runat="server">
+</asp:Content>
+<asp:Content ID="Content6" ContentPlaceHolderID="TitleParaHolder" runat="server">
+</asp:Content>
+<asp:Content ID="Content7" ContentPlaceHolderID="Title2PlaceHolder" runat="server">
+</asp:Content>
+<asp:Content ID="Content8" ContentPlaceHolderID="Title2ListHolder" runat="server">
+</asp:Content>
+<asp:Content ID="Content9" ContentPlaceHolderID="Title3PlaceHolder" runat="server">
+</asp:Content>
+<asp:Content ID="Content10" ContentPlaceHolderID="Title3ListHolder" runat="server">
+</asp:Content>
--- TestEngineData/MasterPage.Master.designer.cs
+++ TestEngineData/MasterPage.Master.designer.cs
@@ -20,5 +20,95 @@ namespace TestEngineData {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// MainHeaderPlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainHeaderPlaceHolder;
+        
+        /// <summary>
+        /// CenterPlcaeHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder CenterPlcaeHolder1;
+        
+        /// <summary>
+        /// CenterPlaceHolderList control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder CenterPlaceHolderList;
+        
+        /// <summary>
+        /// CenterPlaceHolder2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder CenterPlaceHolder2;
+        
+        /// <summary>
+        /// TitlePlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder TitlePlaceHolder;
+        
+        /// <summary>
+        /// TitleParaHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder TitleParaHolder;
+        
+        /// <summary>
+        /// Title2PlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Title2PlaceHolder;
+        
+        /// <summary>
+        /// Title2ListHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Title2ListHolder;
+        
+        /// <summary>
+        /// Title3PlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Title3PlaceHolder;
+        
+        /// <summary>
+        /// Title3ListHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder Title3ListHolder;
     }
 }
